1f3fc7afd1526b176c80677957710a36834876c2
yicizhang00 <zhangyici2000@outlook.com>
2025-08-12 01:58:05 +0800

    Site updated: 2025-08-12 01:58:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/12/hello-world/"];function toRandomPost(){
+var posts=["2025/08/12/hello-world/","2025/08/12/test2/","2025/08/12/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

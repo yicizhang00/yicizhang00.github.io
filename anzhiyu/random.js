ca67c9b148b7948ec4a7af3b9f3a4bc62110e6a8
yicizhang00 <zhangyici2000@outlook.com>
2025-08-25 02:29:44 +0800

    Site updated: 2025-08-25 02:29:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/12/hello-world/","2025/08/12/test2/","2025/08/12/test/"];function toRandomPost(){
+var posts=["2025/08/12/数据库/","2025/08/12/hello-world/","2025/08/12/数据结构/","2025/08/18/math/","2025/08/12/深入Linux内核/","2025/08/12/线段树/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
